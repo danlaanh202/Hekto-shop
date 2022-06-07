9347b32912dbd05da47f7bba84410f9f88977eb6
unknown <mushroomlazy259@gmail.com>
2022-06-07 12:28:34 +0700

    fix some bug

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import "./index.scss";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./utils/constants";
+import { store, persistor } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>
+  <Provider>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </Provider>
 );
